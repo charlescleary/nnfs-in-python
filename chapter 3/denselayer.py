06c68deb63850848d6366ee6f8b26a268f889ff1
Charles Cleary <charlescleary1@gmail.com>
2022-12-21 21:47:36 -0500

    NNFS Chapter 3

--- chapter 3/denselayer.py
+++ chapter 3/denselayer.py
@@ -1,3 +1,5 @@
+"""_summary_
+"""
 import numpy as np
 import nnfs
 from nnfs.datasets import spiral_data
@@ -5,15 +7,28 @@ from nnfs.datasets import spiral_data
 nnfs.init()
 
 # Dense Layer
-class Layer_Dense:
+class LayerDense:
+    """_summary_"""
+
     # Layer initialization
     def __init__(self, n_inputs, n_neurons):
+        """_summary_
+
+        Args:
+            n_inputs (_type_): _description_
+            n_neurons (_type_): _description_
+        """
         # Initilalize weights and biases
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
 
     # Forward pass
     def forward(self, inputs):
+        """_summary_
+
+        Args:
+            inputs (_type_): _description_
+        """
         # Calculate output values from inputs, weight and biases
         self.outputs = np.dot(inputs, self.weights) + self.biases
 
@@ -22,7 +37,7 @@ class Layer_Dense:
 X, y = spiral_data(samples=100, classes=3)
 
 # Create Dense layer with 2 inputs features and 3 output values
-dense1 = Layer_Dense(2, 3)
+dense1 = LayerDense(2, 3)
 
 # Perform a forward pass of our training data through this layer
 dense1.forward(X)
